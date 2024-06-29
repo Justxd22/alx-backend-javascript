11bdc63fe8f95346704920e30e23ce4300393db6
justxd22 <xdjust18@gmail.com>
2024-06-29 18:16:12 +0300

    fixes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(Promise) {
   return Promise
-    .then(() => ({ status: 200, body: 'Success' }))
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
