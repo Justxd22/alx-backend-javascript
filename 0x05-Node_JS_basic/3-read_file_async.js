68674fcfc48b770ae7e0443c3b012ac3c74db74f
justxd22 <xdjust18@gmail.com>
2024-08-21 04:32:10 +0300

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -7,26 +7,31 @@ const countStudents = async (db) => {
       throw new Error('Cannot load the database');
     }
     const data = await fs.readFile(db, 'utf8');
-    const lines = data.split('\n');
+    const lines = data.trim().split('\n');
 
-    const students = lines.slice(1);
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-    const fields = {};
+    for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
+        }
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
 
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
-    });
-
-    const totalStudents = students.length;
+    const totalStudents = Object
+      .values(studentGroups)
+      .reduce((pre, cur) => (pre || []).length + cur.length);
     console.log(`Number of students: ${totalStudents}`);
-
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
     }
     return Promise.resolve();
   } catch (err) {
