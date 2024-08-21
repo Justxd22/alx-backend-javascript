2bf93db94b2193e5a1c5cdc638acf6c8057e6f88
justxd22 <xdjust18@gmail.com>
2024-08-21 03:10:31 +0300

    s

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 const fs = require('fs').promises;
 
 async function countStudents(db) {
--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 const http = require('http');
 const fs = require('fs');
 
